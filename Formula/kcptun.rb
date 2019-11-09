fa6b0fe0e582637e9b11844561ea819719612cdf
Kyle Fang <zhigang1992@gmail.com>
2019-11-09 23:49:02 +0800

    feat: add services

--- Formula/kcptun.rb
+++ Formula/kcptun.rb
@@ -9,6 +9,42 @@ class Kcptun < Formula
   def install
     bin.install 'client_darwin_amd64' => 'kcptun-client'
     bin.install 'server_darwin_amd64' => 'kcptun-server'
+
+    (buildpath/"kcptun-client.json").write <<~EOS
+      {
+        "remoteaddr": "20.40.13.13:4000",
+        "localaddr": ":8388",
+        "mode": "fast3",
+        "nocomp": true,
+        "autoexpire": 900,
+        "sockbuf": 16777217,
+        "dscp": 46
+      }
+    EOS
+    etc.install "kcptun-client.json"
+
+  end
+
+  def plist; <<~EOS
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+      <dict>
+        <key>Label</key>
+        <string>#{plist_name}</string>
+        <key>ProgramArguments</key>
+        <array>
+          <string>#{opt_bin}/kcptun-client</string>
+          <string>-c</string>
+          <string>#{etc}/kcptun-client.json</string>
+        </array>
+        <key>RunAtLoad</key>
+        <true/>
+        <key>KeepAlive</key>
+        <true/>
+      </dict>
+    </plist>
+  EOS
   end
 
   test do
